42d002d114641e0cb519a7a3a92c5e0fe47e2392
ProgrammingPete <peter.parianos@yahoo.com>
2023-06-09 17:34:42 -0400

    small fix

--- src/content_option.js
+++ src/content_option.js
@@ -2,7 +2,7 @@ import mainImg from "./assets/images/main-img.jpg";
 import img1 from "./assets/images/img1.jpg";
 import img2 from "./assets/images/img2.jpg";
 import img3 from "./assets/images/img3.jpg";
-import img4 from "./assets/images/img4.jpg";
+import img4 from "./assets/images/img5.jpg";
 
 const logotext = "GRACE GUANDU";
 const meta = {
