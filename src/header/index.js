4d2118e8cb1e4c9c651fbbfb1d2aac15c074dfde
ProgrammingPete <peter.parianos@yahoo.com>
2023-06-09 00:19:01 -0400

    style changes

--- src/header/index.js
+++ src/header/index.js
@@ -4,7 +4,9 @@ import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import { logotext ,socialprofiles } from "../content_option";
 import Themetoggle from "../components/themetoggle";
-
+import {
+  FaInstagram,
+} from "react-icons/fa";
 const Headermain = () => {
   const [isActive, setActive] = useState("false");
 
@@ -52,9 +54,9 @@ const Headermain = () => {
           </div>
           <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
             <div className="d-flex">
-            <a href={socialprofiles.instagram}>Facebook</a>
+              <a href={socialprofiles.instagram}><FaInstagram /></a>
             </div>
-            <p className="copyright m-0">copyright __ {logotext}</p>
+            <p className="copyright m-0"> {logotext}</p>
           </div>
         </div>
       </header>
