8a8e16cd704d9ca22bbd9f61d9fe43ec195d0c81
ProgrammingPete <peter.parianos@yahoo.com>
2023-06-09 00:32:00 -0400

    change jpg file name to see if it fixes the issue

--- src/content_option.js
+++ src/content_option.js
@@ -1,8 +1,8 @@
 import mainImg from "./assets/images/main-img.jpg";
-import img1 from "./assets/images/img1.jpeg";
-import img2 from "./assets/images/img2.jpeg";
-import img3 from "./assets/images/img3.jpeg";
-import img4 from "./assets/images/img4.JPEG";
+import img1 from "./assets/images/img1.jpg";
+import img2 from "./assets/images/img2.jpg";
+import img3 from "./assets/images/img3.jpg";
+import img4 from "./assets/images/img4.JPG";
 
 const logotext = "GRACE GUANDU";
 const meta = {
